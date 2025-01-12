8177b621be156ed6e958a3cbcdd00ea29631c40f
Mehedi Hasan Shihab <sbshihab2000@gmail.com>
2025-01-12 21:52:14 +0600

    Add user_input and Type Casting

---
+++ user_input.py
@@ -0,0 +1,25 @@
+name=input("Enter Your Name: ")
+print(name)
+
+name=input("Enter Your Name: ")
+age=input("Enter Your Age: ")
+print(f"My Name is {name}, My Age is{age}")
+
+#Type Casting
+
+number_1=int(input("Enter 1st Number: "))
+number_2=int(input("Enter 2st Number: "))
+
+print(number_1 + number_2)
+
+#Converting a Float to an Integer
+x = 5.7
+y = int(x)  
+print(y)  # Output: 5
+
+#Converting a String to an Integer
+x = "25"
+y = int(x)  
+print(y)  # Output: 25
+
+print(str(20)+"20")
